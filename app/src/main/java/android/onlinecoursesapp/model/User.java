5d60a006e59c682b5769a31127c845fb8f6e9667
kaytervn <kienductrong@gmail.com>
2024-05-15 20:07:03 +0700

    sửa tên model

---
+++ app/src/main/java/android/onlinecoursesapp/model/User.java
@@ -0,0 +1,73 @@
+package android.onlinecoursesapp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("id")
+    String id;
+
+    @SerializedName("picture")
+    String picture;
+
+    @SerializedName("name")
+    String name;
+
+    @SerializedName("email")
+    String email;
+
+    @SerializedName("password")
+    String password;
+
+    public User(String id, String picture, String name, String email, String password) {
+        this.id = id;
+        this.picture = picture;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
